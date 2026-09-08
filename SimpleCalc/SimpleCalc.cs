c8fd4c8f522d98a00d5037a3a338023fc64665e9
Eli Harrod <KuroKusho77@outlook.com>
2026-09-08 09:07:49 -0400

    fixed a small bug in the code that i founf when working on lab 4

--- SimpleCalc/SimpleCalc.cs
+++ SimpleCalc/SimpleCalc.cs
@@ -36,8 +36,8 @@ namespace SimpleCalc
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            try
-            {
+
+            
                 decimal operand1 = Convert.ToDecimal(txtOperand1.Text);
                 string operator1 = txtOperator.Text;
                 decimal operand2 = Convert.ToDecimal(txtOperand2.Text);
@@ -47,17 +47,8 @@ namespace SimpleCalc
                 txtResult.Text = result.ToString("F4");
 
                 txtOperand1.Focus();
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Please enter valid numbers.");
-                txtOperand1.Focus();
-            }
-            catch (DivideByZeroException)
-            {
-                MessageBox.Show("Cannot divide by zero.");
-                txtOperand2.Focus();
-            }
+            
+            
         }
 
         private void TextBox_TextChanged(object sender, EventArgs e)
